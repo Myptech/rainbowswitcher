8a307b0bec03270d6bf071a9de8fb1640c678068
Myptech <actaukee@gmail.com>
2025-03-09 10:59:21 +0500

    Исправлен index.html

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 
 export default defineConfig({
-  base: "/rainbowswitcher/", // 👈 Имя репозитория на GitHub!
+  base: "/rainbowswitcher/", // 👈 Поменяй на имя своего репозитория!
   plugins: [react()],
 });
