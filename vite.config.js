9ad2e48642184de1981b4667bbe6e76c3d1cc8fc
Myptech <actaukee@gmail.com>
2025-03-09 14:31:57 +0500

    Понижение React до 18 для совместимости

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react-swc";
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
 
 export default defineConfig({
-  base: "/rainbowswitcher/", // ✅ Убедись, что тут правильно указано имя репозитория!
-  plugins: [react()], // ❌ Убрали Vue, т.к. он не нужен!
+  base: "/rainbowswitcher/", // ✅ Обязательно укажи имя репозитория!
+  plugins: [react()],
 });
