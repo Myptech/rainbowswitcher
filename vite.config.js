a78341d477ee6818f9068191ed97af9ffa8feca5
Myptech <actaukee@gmail.com>
2025-03-09 10:56:13 +0500

    Исправления

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
 import { defineConfig } from "vite";
-import vue from "@vitejs/plugin-vue";
 import react from "@vitejs/plugin-react-swc";
 
 export default defineConfig({
-  base: "/rainbowswitcher/", // ⚠️ Подставь имя своего репозитория!
-  plugins: [vue(), react()],
+  base: "/rainbowswitcher/", // 👈 Имя репозитория на GitHub!
+  plugins: [react()],
 });
